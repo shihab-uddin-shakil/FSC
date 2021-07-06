57ec4aa9636b6b095298657ba57d09072527851e
Shihab Uddin <62871586+shihab-uddin24@users.noreply.github.com>
2021-07-06 22:38:44 +0600

    Update DashboardController.cs

--- MFCS/Controllers/DashboardController.cs
+++ MFCS/Controllers/DashboardController.cs
@@ -30,10 +30,10 @@ namespace MFCS.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(FormCollection collection)
+        public ActionResult Index(DateTime from ,DateTime to)
         {
-            try
-            {
+          //  try
+          //  {
                 var profit = context.Transactions.Sum(e => e.Profit);
                 var sold = context.Transactions.Sum(e => e.Sold);
                 var buye = context.Transactions.Sum(e => e.Buy);
@@ -42,16 +42,24 @@ namespace MFCS.Controllers
                 ViewBag.tsold = sold;
                 ViewBag.tbuye = buye;
                 ViewBag.tavilable = avilable;
+               
                 // DateTime FromDate = Convert.ToDateTime(collection['Form']);
-                var dprofit = context.Transactions.Where(e => e.Date == collection["Form"] && e.Date == collection["To"]).Sum(e => e.Profit);
-                ViewBag.dtprofit ="This Date Profit:  "+ dprofit;
+                var dprofit = context.Transactions.ToList().Where(e =>Convert.ToDateTime(e.Date) >= from && Convert.ToDateTime(e.Date) <= to).Sum(e => e.Profit);
+            // ViewBag.dtprofit =from+ "To " + to+". This Dat Total Profit:  "+ dprofit +"Taka";
+              ViewBag.dte = from + "To " + to + " History:";
+            ViewBag.a = "Total Profit";
+            ViewBag.b = "Total Sold";
+            ViewBag.c = "Total Bought";
+            ViewBag.d = "Taka";
+               ViewBag.dtprofit = dprofit;
+              var dsold = context.Transactions.ToList().Where(e => Convert.ToDateTime(e.Date) >= from && Convert.ToDateTime(e.Date) <= to).Sum(e => e.Sold);
+               ViewBag.dtsale =  dsold;
+               var dbuy = context.Transactions.ToList().Where(e => Convert.ToDateTime(e.Date) >= from && Convert.ToDateTime(e.Date) <= to).Sum(e => e.Buy);
+                ViewBag.datebuy = dbuy ;
 
-                return View();
-            }
-            catch
-            {
-                return View();
-            }
+
+            return View();
+         
         }
     }
 }
